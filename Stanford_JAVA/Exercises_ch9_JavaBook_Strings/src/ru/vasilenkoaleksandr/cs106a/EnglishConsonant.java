a5755f1990e9a11d53297df69ef19b1e274a5681
VASILENKO <admin@admin-PC>
2016-05-10 16:36:08 +0300

    Старт работ по упражнениям в главе 9 (строки и чары)

---
+++ Stanford_JAVA/Exercises_ch9_JavaBook_Strings/src/ru/vasilenkoaleksandr/cs106a/EnglishConsonant.java
@@ -0,0 +1,18 @@
+/**
+ * This class define the exercise 1
+ * @author vasap87*/
+
+package ru.vasilenkoaleksandr.cs106a;
+
+import acm.program.ConsoleProgram;
+
+public class EnglishConsonant extends ConsoleProgram {
+
+	/*default value*/
+	private static final long serialVersionUID = 1L;
+
+	
+	public void run(){
+		
+	}
+}
