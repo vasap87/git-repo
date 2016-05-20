a6d0c547b15d39ee25e7811ff786bf85e5ba8410
vasilenko.aleksandr <vasilenko.aleksandr@otr.ru>
2016-05-20 12:35:39 +0300

    Сделано задание по уроку 5 GB

---
+++ Java1/src/ru/vasilenkoaleksandr/geekbrains/java1/homework5/Animals.java
@@ -0,0 +1,11 @@
+package ru.vasilenkoaleksandr.geekbrains.java1.homework5;
+
+public abstract class Animals {
+	
+	abstract public void run(); //абстрактный бег
+	
+	abstract public void swim();//абстрактное плавание
+	
+	abstract public void jump(double i);//абстрактный прыжок на высоту i
+
+}
---
+++ Java1/src/ru/vasilenkoaleksandr/geekbrains/java1/homework5/HomeWork.java
@@ -0,0 +1,27 @@
+package ru.vasilenkoaleksandr.geekbrains.java1.homework5;
+
+public class HomeWork {
+
+	public static void main(String[] args) {
+		
+		//Создадим массив животных
+		Animals [] arrAnimals = new Animals[3];
+		arrAnimals[0] = new Dog();
+		arrAnimals[1] = new Cat();
+		arrAnimals[2] = new Horse();
+		
+		for (double j = 0.5; j < 3; j+=0.5) {//высота препятсвия
+			//посмотрим, как животные выполняют ту или иную команду.
+			for (int i = 0; i < arrAnimals.length; i++) {
+				arrAnimals[i].run();
+				arrAnimals[i].swim();
+				arrAnimals[i].jump(j);
+				System.out.println();
+			}
+		}
+		
+
+
+	}
+
+}
