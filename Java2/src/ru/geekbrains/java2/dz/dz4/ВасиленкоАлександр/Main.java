5c6950844c65599441da3d3eb8b7f96916221497
Василенко Александр Петрович <vasilenko.aleksandr@otr.ru>
2016-06-20 15:43:21 +0300

    задание 4 по курсу Java2

--- Java2/src/ru/geekbrains/java2/dz/dz4/ВасиленкоАлександр/Main.java
+++ Java2/src/ru/geekbrains/java2/dz/dz4/ВасиленкоАлександр/Main.java
@@ -1,11 +1,10 @@
 package ru.geekbrains.java2.dz.dz4.ВасиленкоАлександр;
 
 /**
- * Created by vasilenko.aleksandr on 17.06.2016.
- * Задание по уроку 4 курса Java2
+ * Created by vasilenko.aleksandr on 20.06.2016.
  */
 public class Main {
     public static void main(String[] args) {
-        Chat.getInstance();
+        LoginJFrame login = new LoginJFrame();
     }
 }
