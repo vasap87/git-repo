57f205f1c3321fa2e4e26eda23301cf250273f13
VASILENKO <admin@admin-PC>
2016-06-20 15:45:14 +0300

    задание 4 по курсу Java2

---
+++ MyProjects/src/ru/vasilenkoaleksandr/myprojects/Chat/Main.java
@@ -0,0 +1,10 @@
+package ru.vasilenkoaleksandr.myprojects.Chat;
+
+/**
+ * Created by vasilenko.aleksandr on 20.06.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        LoginJFrame login = new LoginJFrame();
+    }
+}
