2c716ba2b2cb8b85e495c49d40bf633d5e29861f
vasilenko.aleksandr <vasilenko.aleksandr@otr.ru>
2016-06-21 18:48:09 +0300

    Threads experiments

--- MyProjects/src/ru/vasilenkoaleksandr/myprojects/BlackJack/JField.java
+++ MyProjects/src/ru/vasilenkoaleksandr/myprojects/BlackJack/JField.java
@@ -13,103 +13,112 @@ public class JField extends JFrame {
     private ArrayList<Card> dealer;
     private JPanel dealerCards;
     private JPanel playerCards;
-    private static final Dimension BUTTONS = new Dimension(150,30);
-    private static final Dimension PANELS = new Dimension(200,100);
+    private int scorePlayer;
+    private int scoreDealer;
+    private static final Dimension BUTTONS = new Dimension(150, 30);
+    private static final Dimension PANELS = new Dimension(200, 140);
 
-    public JField(){
+    public JField() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("BlackJack");
-        setSize(400,400);
+        setSize(400, 400);
         GridBagLayout layout = new GridBagLayout();
         GridBagConstraints c = new GridBagConstraints();
         setLayout(layout);
 
         JLabel dealerLabel = new JLabel("Диллер");
-        c.gridx=0;
-        c.gridy=0;
-        c.gridwidth=2;
-        add(dealerLabel,c);
+        c.gridx = 0;
+        c.gridy = 0;
+        c.gridwidth = 2;
+        add(dealerLabel, c);
 
         //карты диллера
         dealerCards = new JPanel();
-        dealerCards.setLayout(new FlowLayout(FlowLayout.CENTER,5,5));
+        dealerCards.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
         dealerCards.setPreferredSize(PANELS);
         dealer = new ArrayList<Card>();
         dealer.add(FabricaCard.getInstance().getRandomCard());
-        dealer.add(FabricaCard.getInstance().getRandomCard());
-        for (Card card: dealer ) {
-            dealerCards.add((Component) card);
-        }
+        printCards(dealer, dealerCards);
 
         addDealerCards();
-        c.gridx=0;
-        c.gridy=1;
-        c.gridwidth=2;
-        add(dealerCards,c);
+        c.gridx = 0;
+        c.gridy = 1;
+        c.gridwidth = 2;
+        add(dealerCards, c);
 
         //кнопки игрока
         JButton moreButton = new JButton("Ещё!");
         moreButton.setPreferredSize(BUTTONS);
         moreButton.addActionListener(e -> {
             player.add(FabricaCard.getInstance().getRandomCard());
-            printCards(player);
+            printCards(player, playerCards);
         });
-        c.gridy=2;
-        c.gridx=0;
-        c.gridwidth=1;
+        c.gridy = 2;
+        c.gridx = 0;
+        c.gridwidth = 1;
         c.fill = GridBagConstraints.BOTH;
-        add(moreButton,c);
+        add(moreButton, c);
 
         JButton pasButton = new JButton("Хватит!");
         pasButton.setPreferredSize(BUTTONS);
-        c.gridy=2;
-        c.gridx=1;
-        c.gridwidth=1;
+        c.gridy = 2;
+        c.gridx = 1;
+        c.gridwidth = 1;
         c.fill = GridBagConstraints.BOTH;
-        add(pasButton,c);
+        add(pasButton, c);
 
         //Карты игрока
         playerCards = new JPanel();
-        playerCards.setLayout(new FlowLayout(FlowLayout.CENTER,5,5));
+        playerCards.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
         playerCards.setPreferredSize(PANELS);
         player = new ArrayList<Card>();
         player.add(FabricaCard.getInstance().getRandomCard());
         player.add(FabricaCard.getInstance().getRandomCard());
-        printCards(player);
+        printCards(player, dealerCards);
+        getSum(player);
 
-        c.gridy=3;
-        c.gridx=0;
-        c.gridwidth=2;
-        c.weightx=0;
-        add(playerCards,c);
+        c.gridy = 3;
+        c.gridx = 0;
+        c.gridwidth = 2;
+        c.weightx = 0;
+        add(playerCards, c);
 
 
-        JLabel plalerLabel = new JLabel("Игрок");
-        c.gridx=0;
-        c.gridy=4;
-        c.gridwidth=2;
+        JLabel playerLabel = new JLabel("Игрок");
+        c.gridx = 0;
+        c.gridy = 4;
+        c.gridwidth = 2;
         c.fill = GridBagConstraints.NONE;
-        add(plalerLabel,c);
+        add(playerLabel, c);
 
         setVisible(true);
 
 
-
-
     }
 
-    private void printCards(ArrayList<Card> player) {
-        playerCards.removeAll();
-        for (Card card: player ) {
-            playerCards.add((Component) card);
+    private void getSum(ArrayList<Card> arrayList) {
+        int sum = 0;
+        for (Card card : arrayList) {
+            sum += card.getRank();
         }
-        playerCards.updateUI();
+        if(sum == 21){
+            JOptionPane.showInternalMessageDialog(getContentPane(),"Невероятное везение!\nBlackJack при раздаче!!","Победа",JOptionPane.INFORMATION_MESSAGE);
+        }else if(sum>21){
+            JOptionPane.showInternalMessageDialog(getContentPane(),"Проигрыш!\nпопробуй ещё раз","Проигрыш",JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+
+    private void printCards(ArrayList<Card> player, JPanel panelCards) {
+        panelCards.removeAll();
+        for (Card card : player) {
+            panelCards.add((Component) card);
+        }
+        panelCards.updateUI();
     }
 
 
     private void addDealerCards() {
         //добавление карт диллеру
-
 
     }
 }
--- MyProjects/src/ru/vasilenkoaleksandr/myprojects/Threads/Main.java
+++ MyProjects/src/ru/vasilenkoaleksandr/myprojects/Threads/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
         float[] arr = new float[SIZE];
         Arrays.fill(arr, 1.0f);
-        firstMethod(arr);
+        //firstMethod(arr);
         secondMethod(arr);
     }
 
@@ -30,51 +30,99 @@ public class Main {
         //режем массив на 2 равные части
         //засекаем время
         long a = System.currentTimeMillis();
-        float[] arr1 = new float[SIZE / 4];
-        float[] arr2 = new float[SIZE / 4];
-        float[] arr3 = new float[SIZE / 4];
-        float[] arr4 = new float[SIZE / 4];
+        float[] arr1 = new float[SIZE / 10];
+        float[] arr2 = new float[SIZE / 10];
+        float[] arr3 = new float[SIZE / 10];
+        float[] arr4 = new float[SIZE / 10];
+        float[] arr5 = new float[SIZE / 10];
+        float[] arr6 = new float[SIZE / 10];
+        float[] arr7 = new float[SIZE / 10];
+        float[] arr8 = new float[SIZE / 10];
+        float[] arr9 = new float[SIZE / 10];
+        float[] arr10 = new float[SIZE / 10];
 
-        System.arraycopy(arr, 0, arr1, 0, SIZE / 4);
-        System.arraycopy(arr, SIZE / 4, arr2, 0, SIZE / 4);
-        System.arraycopy(arr, SIZE / 2, arr3, 0, SIZE / 4);
-        System.arraycopy(arr, SIZE / 4 * 3, arr3, 0, SIZE / 4);
+        System.arraycopy(arr, 0, arr1, 0, SIZE / 10);
+        System.arraycopy(arr, SIZE / 10, arr2, 0, SIZE / 10);
+        System.arraycopy(arr, SIZE / 10 * 2, arr3, 0, SIZE / 10);
+        System.arraycopy(arr, SIZE / 10 * 3, arr4, 0, SIZE / 10);
+        System.arraycopy(arr, SIZE / 10 * 4, arr5, 0, SIZE / 10);
+        System.arraycopy(arr, SIZE / 10 * 5, arr6, 0, SIZE / 10);
+        System.arraycopy(arr, SIZE / 10 * 6, arr7, 0, SIZE / 10);
+        System.arraycopy(arr, SIZE / 10 * 7, arr8, 0, SIZE / 10);
+        System.arraycopy(arr, SIZE / 10 * 8, arr9, 0, SIZE / 10);
+        System.arraycopy(arr, SIZE / 10 * 9, arr10, 0, SIZE / 10);
 
         //создаём 2 объектра реализующих интерфейс Runnable
         SimpleThread sTh1 = new SimpleThread(arr1);
         SimpleThread sTh2 = new SimpleThread(arr2);
         SimpleThread sTh3 = new SimpleThread(arr3);
         SimpleThread sTh4 = new SimpleThread(arr4);
+        SimpleThread sTh5 = new SimpleThread(arr5);
+        SimpleThread sTh6 = new SimpleThread(arr6);
+        SimpleThread sTh7 = new SimpleThread(arr7);
+        SimpleThread sTh8 = new SimpleThread(arr8);
+        SimpleThread sTh9 = new SimpleThread(arr9);
+        SimpleThread sTh10 = new SimpleThread(arr10);
 
         //создаём 2 объекта класса Thread
         Thread firstThread = new Thread(sTh1);
         Thread secondThread = new Thread(sTh2);
         Thread thirdThread = new Thread(sTh3);
         Thread fourthThread = new Thread(sTh4);
-        firstThread.setPriority(Thread.NORM_PRIORITY);
-        secondThread.setPriority(Thread.NORM_PRIORITY);
-        thirdThread.setPriority(Thread.NORM_PRIORITY);
-        fourthThread.setPriority(Thread.NORM_PRIORITY);
+        Thread fifthThread = new Thread(sTh5);
+        Thread sixthThread = new Thread(sTh6);
+        Thread seventhThread = new Thread(sTh7);
+        Thread eighthThread = new Thread(sTh8);
+        Thread ninethThread = new Thread(sTh9);
+        Thread tenthThread = new Thread(sTh10);
+//        firstThread.setPriority(Thread.NORM_PRIORITY);
+//        secondThread.setPriority(Thread.NORM_PRIORITY);
+//        thirdThread.setPriority(Thread.NORM_PRIORITY);
+//        fourthThread.setPriority(Thread.NORM_PRIORITY);
 
         firstThread.start();
         secondThread.start();
         thirdThread.start();
         fourthThread.start();
+        fifthThread.start();
+        sixthThread.start();
+        seventhThread.start();
+        eighthThread.start();
+        ninethThread.start();
+        tenthThread.start();
         //ждём когда выполнятся оба потока
         long b;
         while (true) {
             if (firstThread.getState().equals(Thread.State.TERMINATED)
                     && secondThread.getState().equals(Thread.State.TERMINATED)
                     && thirdThread.getState().equals(Thread.State.TERMINATED)
-                    && fourthThread.getState().equals(Thread.State.TERMINATED)) {
+                    && fourthThread.getState().equals(Thread.State.TERMINATED)
+                    && fifthThread.getState().equals(Thread.State.TERMINATED)
+                    && sixthThread.getState().equals(Thread.State.TERMINATED)
+                    && seventhThread.getState().equals(Thread.State.TERMINATED)
+                    && eighthThread.getState().equals(Thread.State.TERMINATED)
+                    && ninethThread.getState().equals(Thread.State.TERMINATED)
+                    && tenthThread.getState().equals(Thread.State.TERMINATED)) {
                 arr1 = sTh1.getArr();
                 arr2 = sTh2.getArr();
                 arr3 = sTh3.getArr();
                 arr4 = sTh4.getArr();
-                System.arraycopy(arr1, 0, arr, 0, SIZE / 4);
-                System.arraycopy(arr2, 0, arr, SIZE / 4, SIZE / 4);
-                System.arraycopy(arr3, 0, arr, SIZE / 2, SIZE / 4);
-                System.arraycopy(arr4, 0, arr, SIZE / 4 * 3, SIZE / 4);
+                arr5 = sTh5.getArr();
+                arr6 = sTh6.getArr();
+                arr7 = sTh7.getArr();
+                arr8 = sTh8.getArr();
+                arr9 = sTh9.getArr();
+                arr10 = sTh10.getArr();
+                System.arraycopy(arr1, 0, arr, 0, SIZE / 10);
+                System.arraycopy(arr2, 0, arr, SIZE / 10, SIZE / 10);
+                System.arraycopy(arr3, 0, arr, SIZE / 10 * 2, SIZE / 10);
+                System.arraycopy(arr4, 0, arr, SIZE / 10 * 3, SIZE / 10);
+                System.arraycopy(arr5, 0, arr, SIZE / 10 * 4, SIZE / 10);
+                System.arraycopy(arr6, 0, arr, SIZE / 10 * 5, SIZE / 10);
+                System.arraycopy(arr7, 0, arr, SIZE / 10 * 6, SIZE / 10);
+                System.arraycopy(arr8, 0, arr, SIZE / 10 * 7, SIZE / 10);
+                System.arraycopy(arr9, 0, arr, SIZE / 10 * 8, SIZE / 10);
+                System.arraycopy(arr10, 0, arr, SIZE / 10 * 9, SIZE / 10);
                 b = System.currentTimeMillis() - a;
                 break;
             }
