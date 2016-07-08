0c8616fba5246fe430f72d0a875815dff2e3d10d
vasilenko.aleksandr <vasilenko.aleksandr@otr.ru>
2016-07-08 19:27:31 +0300

    J3L1 заготовка 0.2

--- Java3/Lesson1/src/main/java/ru/gb/AlexVasilenko/java3/lesson1/Stack.java
+++ Java3/Lesson1/src/main/java/ru/gb/AlexVasilenko/java3/lesson1/Stack.java
@@ -1,5 +1,7 @@
 package ru.gb.AlexVasilenko.java3.lesson1;
 
+import java.util.Collection;
+
 /**
  * Created by vasilenko.aleksandr on 08.07.2016.
  */
@@ -8,6 +10,15 @@ public interface Stack<E> {
     void push(E element) throws StackExeption;
     //вывести и удалить элемент их верха коллекции
     E pop() throws StackExeption;
-
-
+    //вывести и не удалять верхний эллемент
+    E peek();
+    //вывести количество элементов стека
+    int getSize();
+    //вывести
+    boolean isEmpty();
+    boolean isFull();
+    // добавить все элементы @src в стек
+    void pushAll(Collection<? extends E> src) throws StackExeption;
+    // вывести из стека все элементы в коллекцию  @dst
+    void popAll(Collection<? super E> dst) throws StackExeption;
 }
