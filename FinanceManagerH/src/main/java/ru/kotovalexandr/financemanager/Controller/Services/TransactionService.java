fff0c3284b45a66c832fc05f9b64bf254b4656af
vasap87 <a200x@bk.ru>
2016-10-06 18:27:04 +0300

    Hibernate FinanceManager added bd file

--- FinanceManagerH/src/main/java/ru/kotovalexandr/financemanager/Controller/Services/TransactionService.java
+++ FinanceManagerH/src/main/java/ru/kotovalexandr/financemanager/Controller/Services/TransactionService.java
@@ -1,11 +1,13 @@
 package ru.kotovalexandr.financemanager.Controller.Services;
 
 
-
 import ru.kotovalexandr.financemanager.Controller.Account.AccountList;
 import ru.kotovalexandr.financemanager.Controller.Transaction.TransactionList;
 import ru.kotovalexandr.financemanager.Dao.DBHelper;
 import ru.kotovalexandr.financemanager.Dao.TransactionDao;
+import ru.kotovalexandr.financemanager.Hibernate.DAO.CommonGenDaoImpl;
+import ru.kotovalexandr.financemanager.Hibernate.DAO.DAOFabric;
+import ru.kotovalexandr.financemanager.Hibernate.DAO.TransactionDaoImpl;
 import ru.kotovalexandr.financemanager.Model.Account;
 import ru.kotovalexandr.financemanager.Model.Transaction;
 import ru.kotovalexandr.financemanager.View.Tools.JList.AccountJList;
@@ -23,55 +25,42 @@ public final class TransactionService {
     private TransactionService() {
     }
 
-    public static void updateList(TransactionJList transactionJList, AccountJList accountJList){
+    public static void updateList(TransactionJList transactionJList, AccountJList accountJList) {
         if (accountJList.isSelectionEmpty()) {
             accountJList.setSelectedIndex(0);
         }
         Account account = (Account) accountJList.getSelectedValue();
         if (account != null) {
-            try {
-                Connection connection = DBHelper.getInstance().getConnection();
-                TransactionDao transactionDao = new TransactionDao(connection);
-                List<Transaction> transactions = transactionDao.getAllByAccout(account);
-                Object arr[] = transactions.toArray();
-                transactionJList.setListData(arr);
-                DBHelper.getInstance().closeConnection();
-            } catch (SQLException e) {
-                e.printStackTrace();
+            TransactionDaoImpl transactionDao = DAOFabric.getTransactionDao();
+            List<Transaction> transactions = transactionDao.getAll();
+            for (Transaction transaction : transactions) {
+                if (transaction.getAccount_id() != account.getId()) transactions.remove(transaction);
             }
+            Object arr[] = transactions.toArray();
+            transactionJList.setListData(arr);
+
         }
     }
 
     public static void remove(Transaction transaction) {
-        try {
-            Connection connection = DBHelper.getInstance().getConnection();
-            TransactionDao transactionDao = new TransactionDao(connection);
-            transactionDao.delete(transaction);
-            TransactionList.getInstance().notifyObservers();
-            AccountList.getInstance().notifyObservers();
-            DBHelper.getInstance().closeConnection();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        CommonGenDaoImpl transactionDao = DAOFabric.getCommonDao();
+        transactionDao.delete(transaction);
+        TransactionList.getInstance().notifyObservers();
+        AccountList.getInstance().notifyObservers();
     }
 
     public static void addOrUpdateTransaction(Transaction transaction, int operID) {
-        try {
-            Connection connection = DBHelper.getInstance().getConnection();
-            TransactionDao transactionDao = new TransactionDao(connection);
-            switch (operID){
-                case 0: {
-                    transactionDao.save(transaction);
-                    break;
-                }
-                case 1: {
-                    transactionDao.update(transaction);
-                    break;
-                }
+        CommonGenDaoImpl transactionDao = DAOFabric.getCommonDao();
+        switch (operID) {
+            case 0: {
+                transactionDao.save(transaction);
+                break;
             }
-            DBHelper.getInstance().closeConnection();
-        } catch (SQLException e1) {
-            e1.printStackTrace();
+            case 1: {
+                transactionDao.update(transaction);
+                break;
+            }
         }
+
     }
 }
